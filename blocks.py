adab6e32e5ab3f65313de21c23456479bf9dd767
Quentin Curteman <qcurteman@gmail.com>
2018-10-28 20:51:54 -0700

    Initial commit

---
+++ blocks.py
@@ -0,0 +1,44 @@
+
+class Block:
+
+    def __init__(self, ):
+        self.size = '4KB' # TODO: fill this in correclty
+        self.address # ??? maybe have a start address and end address ??
+        
+
+class Superblock(Block):
+
+    def __init__(self, nblocks, ninodeblocks, ninodes):
+        self.magic_number = '0xf0f03410'
+        self.nblocks = nblocks
+        self.ninodeblocks = ninodeblocks
+        self.ninodes = ninodes
+
+
+class Inode():
+
+    # each field is 32-bits
+    def __init__(self, size):
+        self.is_valid = False
+        self.size = size
+        self.direct = [] # points to data blocks
+        self.indirect = 0 # points to an indirect block
+
+
+class InodeBlock(Block):
+    
+    # consists of 128 Inodes
+    def __init__(self, ):
+        pass
+
+
+class IndirectBlock(Block):
+
+    # consists of 1024 4-byte pointers that point to data blocks
+    def __init__(self, ):
+        pass
+
+
+class DataBlock(Block):
+    def __init__(self, ):
+        pass
---
+++ diskpy.py
@@ -0,0 +1,22 @@
+
+class Disk:
+
+    DISK_BLOCK_SIZE = 4096    
+
+    def disk_init(self, filename, nbrOfBlocks):
+        self.BLOCKS = []
+
+    def disk_open(filename):
+        pass
+
+    def disk_read(blockNumber):
+        pass
+
+    def disk_write(blockNumber, byteArray):
+        pass
+
+    def disk_status():
+        pass
+
+    def disk_close():
+        pass
